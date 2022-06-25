9d8347521d4390a4159448382ead7209ae837a4d
DudeUnoob <to.baladev@gmail.com>
2022-06-25 17:47:41 -0500

    pls work

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const SpotifyWebApi = require("spotify-web-api-node")
-const serve = require('./testServer')
 require('dotenv').config();
 const app = express()
 app.use(cors())
